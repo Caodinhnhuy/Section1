a89f458cfbd88b3ac88f258dcaf9a6932034d2aa
Caodinhnhuy <caodinhnhuy@gmail.com>
2025-05-23 21:54:15 +0700

    Add project files.

---
+++ CoffeShopp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoffeShopp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ CoffeShopp/Models/Services/ProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using CoffeeShop.Models.Interfaces;
+
+namespace CoffeeShop.Models.Services
+{
+    public class ProductRepository : IProductRepository
+    {
+        private List<Product> ProductsList = new List<Product>()
+        {
+            new Product{ Id = 1, Name = "America", Price = 25, Detail = "Name product", ImageUrl = "https://index.com", IsTrendingProduct = true },
+            new Product{ Id = 2, Name = "Vietnam", Price = 20, Detail = "Vietnamese product", ImageUrl = "https://index.com", IsTrendingProduct = false },
+            new Product{ Id = 3, Name = "United Kingdom", Price = 15, Detail = "UK product", ImageUrl = "https://index.com", IsTrendingProduct = true }
+        };
+
+        public IEnumerable<Product> GetAllProducts() => ProductsList;
+
+        public Product? GetProductDetail(int id) =>
+            ProductsList.FirstOrDefault(p => p.Id == id);
+
+        public IEnumerable<Product> GetTrendingProducts() =>
+            ProductsList.Where(p => p.IsTrendingProduct);
+    }
+}
